cb3a0a7681b0d030172951c5a88c5c000cc7f8be
Brandon Au <tech.brandonau@gmail.com>
2020-08-16 23:06:34 -0400

    fix: move config files back to the root of the project

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	testMatch: ['**/__tests__/**/*.jsx?'],
 	moduleFileExtensions: ['js', 'jsx'],
-	setupFiles: ['<rootDir>/config/enzyme.config.js'],
+	setupFiles: ['<rootDir>/enzyme.config.js'],
 	coverageDirectory: '<rootDir>/coverage',
 	rootDir: path.resolve(process.cwd())
 };
