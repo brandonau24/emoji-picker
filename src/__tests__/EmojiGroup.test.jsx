01b6aa8f1bf4f3afaaa4326e1ebe2edbdbf27625
Brandon Au <tech.brandonau@gmail.com>
2020-08-22 22:19:53 -0400

    refactor: move props data and subjec to a reusable scope

--- src/__tests__/EmojiGroup.test.jsx
+++ src/__tests__/EmojiGroup.test.jsx
@@ -4,32 +4,35 @@ import EmojiGroup from 'EmojiGroup';
 import Emoji from 'Emoji';
 
 describe('EmojiGroup', () => {
-	it('renders header with group name', () => {
-		const groupName = 'Group 1';
-		const subject = shallow(<EmojiGroup groupName={groupName} group={{ subgroup: [] }} />);
+	const groupName = 'Group 1';
+	const group = {
+		subgroup: [
+			{
+				name: 'emoji1',
+				codepoints: '111'
+			},
+			{
+				name: 'emoji',
+				codepoints: '222'
+			},
+			{
+				name: 'emoji',
+				codepoints: '333'
+			}
+		]
+	};
 
+	let subject;
+
+	beforeEach(() => {
+		subject = shallow(<EmojiGroup groupName={groupName} group={group} />);
+	});
+
+	it('renders header with group name', () => {
 		expect(subject.find('h4').text()).toBe(groupName);
 	});
 
 	it('renders emojis in a subgroup', () => {
-		const group = {
-			subgroup: [
-				{
-					name: 'emoji1',
-					codepoints: '111'
-				},
-				{
-					name: 'emoji',
-					codepoints: '222'
-				},
-				{
-					name: 'emoji',
-					codepoints: '333'
-				}
-			]
-		};
-
-		const subject = shallow(<EmojiGroup groupName="Group 1" group={group} />);
 		const emojis = subject.find(Emoji);
 		expect(emojis).toHaveLength(group.subgroup.length);
 	});
