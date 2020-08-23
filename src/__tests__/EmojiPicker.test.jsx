4390bfc7410496353ca4979a01e43e4d7c317a71
Brandon Au <tech.brandonau@gmail.com>
2020-08-22 22:17:01 -0400

    refactor: move data and subject to a reusable scope

--- src/__tests__/EmojiPicker.test.jsx
+++ src/__tests__/EmojiPicker.test.jsx
@@ -4,28 +4,24 @@ import EmojiPicker from 'EmojiPicker';
 import EmojiGroup from 'EmojiGroup';
 
 describe('EmojiPicker', () => {
+	const data = {
+		version: '13.0',
+		'group-1': {},
+		'group-2': {},
+		'group-3': {}
+	};
+
+	let subject;
+
+	beforeEach(() => {
+		subject = shallow(<EmojiPicker data={data} />);
+	});
+
 	it('creates emoji groups for top level groups', () => {
-		const data = {
-			version: '13.0',
-			'group-1': {},
-			'group-2': {},
-			'group-3': {}
-		};
-
-		const subject = shallow(<EmojiPicker data={data} />);
-
 		expect(subject.find(EmojiGroup)).toHaveLength(3);
 	});
 
 	it('creates footer with Unicode version', () => {
-		const data = {
-			version: '13.0',
-			'group-1': {},
-			'group-2': {},
-			'group-3': {}
-		};
-
-		const subject = shallow(<EmojiPicker data={data} />);
 		const footer = subject.find('#footer');
 
 		expect(footer.text()).toBe(`Built with Unicode Emoji v${data.version}`);
