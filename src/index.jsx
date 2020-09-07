6a77b7e7f15a703d1db2a9191460ec155f88d477
Brandon Au <tech.brandonau@gmail.com>
2020-09-06 22:58:46 -0400

    feat: remove twemoji parsing due to performance.
    Twemoji allows for cross-platform consistency with emojis (especially with flags on Windows), but it requires downloading SVGs/PNGs from their CDN which is a lot of bandwidth that can be saved.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import twemoji from 'twemoji';
 import EmojiPicker from 'EmojiPicker';
 import data from './emoji-data.json';
 
@@ -10,9 +9,3 @@ root.id = 'emoji-picker';
 document.body.append(root);
 
 ReactDOM.render(<EmojiPicker data={data} />, root);
-
-twemoji.parse(root, {
-	ext: '.svg',
-	folder: 'svg',
-	className: 'emoji-img'
-});
