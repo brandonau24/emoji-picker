5aa398b5077d0bbeb93b9ec6c5cc9247d1f67e8f
Brandon Au <tech.brandonau@gmail.com>
2020-09-07 00:24:58 -0400

    feat: readd twemoji parsing to normalize emoji appearance
    Windows renders gendered emojis oddly by putting the gender symbol next to the base emoji.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import twemoji from 'twemoji';
 import EmojiPicker from 'EmojiPicker';
 import data from './emoji-data.json';
 
@@ -9,3 +10,9 @@ root.id = 'emoji-picker';
 document.body.append(root);
 
 ReactDOM.render(<EmojiPicker data={data} />, root);
+
+twemoji.parse(root, {
+	folder: 'svg',
+	ext: '.svg',
+	className: 'emoji-img'
+});
