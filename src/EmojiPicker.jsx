db6fd63d60ba60b01f15c4ee45423ec4afcfd670
Brandon Au <tech.brandonau@gmail.com>
2020-09-06 23:01:06 -0400

    Revert "feat: add and remove click event listeners to document in EmojiPicker"
    This reverts commit 126bba4563762122527bcb58dc15f26e3f6bf460 to redo
    click event handling using React's event handling system which does
    include event delegation.

--- src/EmojiPicker.jsx
+++ src/EmojiPicker.jsx
@@ -2,37 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import EmojiGroup from 'EmojiGroup';
 
-class EmojiPicker extends React.Component {
-	static copyEmoji() {
-		console.log('copyEmoji');
-	}
+const EmojiPicker = ({ data }) => {
+	const groups = [];
 
-	componentDidMount() {
-		document.addEventListener('click', EmojiPicker.copyEmoji);
-	}
+	Object.entries(data).forEach(([key, value]) => {
+		if (key !== 'version') {
+			groups.push(<EmojiGroup key={key} groupName={key} group={value} />);
+		}
+	});
 
-	componentWillUnmount() {
-		document.removeEventListener('click', EmojiPicker.copyEmoji);
-	}
-
-	render() {
-		const { data } = this.props;
-		const groups = [];
-
-		Object.entries(data).forEach(([key, value]) => {
-			if (key !== 'version') {
-				groups.push(<EmojiGroup key={key} groupName={key} group={value} />);
-			}
-		});
-
-		return (
-			<>
-				{groups}
-				<div id="footer">{`Built with Unicode Emoji v${data.version}`}</div>
-			</>
-		);
-	}
-}
+	return (
+		<>
+			{groups}
+			<div id="footer">{`Built with Unicode Emoji v${data.version}`}</div>
+		</>
+	);
+};
 
 EmojiPicker.propTypes = {
 	data: PropTypes.objectOf(PropTypes.oneOfType([
--- src/__tests__/EmojiPicker.test.jsx
+++ src/__tests__/EmojiPicker.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import EmojiPicker from 'EmojiPicker';
 import EmojiGroup from 'EmojiGroup';
 
@@ -25,22 +25,5 @@ describe('EmojiPicker', () => {
 		const footer = subject.find('#footer');
 
 		expect(footer.text()).toBe(`Built with Unicode Emoji v${data.version}`);
-	});
-
-	it('adds click event listener to document after mounting', () => {
-		const addEventListenerSpy = jest.spyOn(document, 'addEventListener');
-
-		// Rerender component since the spy occurs after the beforeEach
-		subject = shallow(<EmojiPicker data={data} />);
-
-		expect(addEventListenerSpy).toBeCalledWith('click', EmojiPicker.copyEmoji);
-	});
-
-	it('removes click event listener to document when unmounting', () => {
-		const removeEventListenerSpy = jest.spyOn(document, 'removeEventListener');
-		const mountedSubject = mount(<EmojiPicker data={data} />);
-
-		mountedSubject.unmount();
-		expect(removeEventListenerSpy).toBeCalledWith('click', EmojiPicker.copyEmoji);
 	});
 });
