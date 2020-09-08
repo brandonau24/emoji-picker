d21b35da1ff985c78be137884630a2cf7d64db2b
Brandon Au <tech.brandonau@gmail.com>
2020-09-07 23:36:12 -0400

    refactor: use twemoji's fromCodePoint function to transform codepoints into emojis

--- src/Emoji/Emoji.jsx
+++ src/Emoji/Emoji.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import twemoji from 'twemoji';
 
 import './Emoji.scss';
 
-const transformCodepoints = (codepoints) => {
-	const transformedCodepoints = codepoints.split(' ').map((codepoint) => `0x${codepoint}`);
-
-	return String.fromCodePoint(...transformedCodepoints);
-};
-
 const Emoji = ({ codepoints, name, onClick }) => {
-	const transformedCodepoints = transformCodepoints(codepoints);
+	const transformedCodepoints = twemoji.convert.fromCodePoint(codepoints);
 
 	return <button type="button" className="emoji" title={name} alt={name} value={transformedCodepoints} onClick={onClick}>{transformedCodepoints}</button>;
 };
