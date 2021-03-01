679f8721338a329229b505e54c2a1abfe8b1e2be
Brandon Au <tech.brandonau@gmail.com>
2021-02-28 19:00:38 -0500

    style: convert px to rem, change color of toggle when on, and add margin on the sides of toggle

--- src/Toggle/Toggle.jsx
+++ src/Toggle/Toggle.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import './Toggle.scss';
 
+// Taken from https://www.w3schools.com/howto/howto_css_switch.asp
 const Toggle = ({ onChange, id, checked, }) => (
 	<label className="switch" htmlFor={id}>
 		<input id={id} type="checkbox" checked={checked} onChange={onChange} />
