878fb1e7a56ac98009a1a6c5e0f5b403be24eccc
Brandon Au <tech.brandonau@gmail.com>
2021-03-01 23:09:45 -0500

    refactor: change search field component from class component to function component with react hooks

--- src/Toolbar/SearchField/SearchField.jsx
+++ src/Toolbar/SearchField/SearchField.jsx
@@ -1,40 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Icon from 'Icon';
 
 import './SearchField.scss';
 
-class SearchField extends React.Component {
-	constructor(props) {
-		super(props);
+const SearchField = ({ onChangeCallback }) => {
+	const [searchValue, setSearchValue] = useState('');
 
-		this.state = {
-			searchValue: ''
-		};
-
-		this.onChange = this.onChange.bind(this);
-	}
-
-	onChange(event) {
-		const { onChangeCallback } = this.props;
-		const searchValue = event.target.value;
-
-		this.setState({ searchValue });
-
+	useEffect(() => {
 		onChangeCallback(searchValue);
-	}
+	});
 
-	render() {
-		const { searchValue } = this.state;
-
-		return (
-			<div className="search-field">
-				<Icon name="search" />
-				<input type="text" value={searchValue} onChange={this.onChange} placeholder="Search for an emoji" />
-			</div>
-		);
-	}
-}
+	return (
+		<div className="search-field">
+			<Icon name="search" />
+			<input type="text" value={searchValue} onChange={(event) => setSearchValue(event.target.value)} placeholder="Search for an emoji" />
+		</div>
+	);
+};
 
 SearchField.propTypes = {
 	onChangeCallback: PropTypes.func.isRequired
