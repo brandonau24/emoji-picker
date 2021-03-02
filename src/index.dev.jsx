f165f48d5d46d47e4408513d63a521c861cf1aaf
Brandon Au <tech.brandonau@gmail.com>
2021-03-02 00:04:10 -0500

    chore: remove twemoji parsing in index development file

--- src/index.dev.jsx
+++ src/index.dev.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import twemoji from 'twemoji';
 import EmojiPicker from 'EmojiPicker';
 import data from './emoji-data.json';
 
 const root = document.querySelector('#emoji-picker');
 
 ReactDOM.render(<EmojiPicker data={data} />, root);
-
-twemoji.parse(root, {
-	folder: 'svg',
-	ext: '.svg',
-	className: 'emoji-img'
-});
