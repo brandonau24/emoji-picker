e6bcffd80b249b3df574023c7ee3b4375a8aa733
Brandon Au <tech.brandonau@gmail.com>
2021-03-03 21:07:22 -0500

    chore(BREAKING): remove enzyme from project

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	testRegex: '/__tests__/.+\.jsx?',
-	setupFilesAfterEnv: ['<rootDir>/enzyme.config.js', '<rootDir>/setupFile.js'],
+	setupFilesAfterEnv: ['<rootDir>/setupFile.js'],
 	coverageDirectory: '<rootDir>/coverage',
 	rootDir: path.resolve(process.cwd()),
 	moduleFileExtensions: ['js', 'jsx'],
